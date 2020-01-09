68d4d67800a0e198fb077f82200f54c2577bbb95
ygj <1204697373@qq.com>
2020-01-09 22:18:50 +0800

    提交User.java

--- gitdemo/src/main/java/com/itheima/domain/User.java
+++ gitdemo/src/main/java/com/itheima/domain/User.java
@@ -19,4 +19,12 @@ public class User {
     public void setAge(String age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
 }
